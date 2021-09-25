dac73ef0840d0878975e49a7be50ee5359e52060
Agus Janardana <bjanardana@gmail.com>
2021-09-26 00:03:17 +0800

    fixing gorm

--- model/record/status.go
+++ model/record/status.go
@@ -7,7 +7,7 @@ import (
 
 type Status struct {
 	Id        int       `json:"id" gorm:"primaryKey,not null, autoIncrement"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" gorm:"type:enum('FREE','PREMIUM');default:'USER'"`
 	UserId    int       `json:"user_id" gorm:"not null"`
 	TokenId   string    `json:"token_id"`
 	CreatedAt time.Time `json:"created_at"`
