4dd874f7ad92a278d52e188bd7b33f910120adaf
Agus Janardana <bjanardana@gmail.com>
2021-09-27 14:07:06 +0800

    fixing typo

--- model/record/status.go
+++ model/record/status.go
@@ -7,7 +7,7 @@ import (
 
 type Status struct {
 	Id        int    `gorm:"primaryKey,not null, autoIncrement"`
-	Name      string `gorm:"type:enum('FREE','PREMIUM');default:'USER'"`
+	Name      string `gorm:"type:enum('FREE','PREMIUM');default:'FREE'"`
 	UserId    int    ` gorm:"not null"`
 	TokenId   string
 	CreatedAt time.Time
